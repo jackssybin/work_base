53af078bf5602ba5810cc30b2a117fcdecad33c1
jackssy <27208461@qq.com>
2019-12-12 17:27:08 +0800

    1.更改打包格式

--- src/main/java/com/jackssy/ZJackssybaseApplication.java
+++ src/main/java/com/jackssy/ZJackssybaseApplication.java
@@ -2,11 +2,19 @@ package com.jackssy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @SpringBootApplication
-public class ZJackssybaseApplication {
+public class ZJackssybaseApplication extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ZJackssybaseApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ZJackssybaseApplication.class, args);
